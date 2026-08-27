7d4bb4dca6b5e5303e9bee33b0503de131ad86fd
Dimas.Priyandi@ag-it.com <masdidi.priyandi97@gmail.com>
2026-08-27 10:48:48 +0700

    update api upload using vercel blob

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -1,47 +1,52 @@
-import { mkdir, writeFile } from "fs/promises";
+import { put } from "@vercel/blob";
 import { NextRequest, NextResponse } from "next/server";
-import path from "path";
 
-export async function POST(request:NextRequest){
-    try{
-        const formData = await request.formData();
+export async function POST(request: NextRequest) {
+  try {
+    const formData = await request.formData();
 
-        const files = formData.getAll("files") as File[];
+    const files = formData.getAll("files") as File[];
 
-        if(!files.length){
-            return NextResponse.json(
-                {error:"No files uploaded"},
-                {status: 400}
-            );
-        }
-
-        const uploadDir = path.join(process.cwd(),"public","uploads");
-        await mkdir(uploadDir,{recursive:true})
-
-        const urls:string[] = [];
-
-        for(const file of files){
-            const bytes = await file.arrayBuffer();
-            const buffer = Buffer.from(bytes);
-
-            const extension = path.extname(file.name);
-            const fileName = `${crypto.randomUUID()}${extension}`;
-            const filePath = path.join(uploadDir,fileName);
-            
-            await writeFile(filePath,buffer);
-
-            urls.push(`/uploads/${fileName}`);
-        }
-
-        return NextResponse.json(
-            {urls}
-        );
-    }catch(err){
-    console.error(err);
-
-        return NextResponse.json(
-        { error: "Upload failed" },
-        { status: 500 }
-        );
+    if (!files.length) {
+      return NextResponse.json(
+        { error: "No files uploaded" },
+        { status: 400 }
+      );
     }
+
+    const urls: string[] = [];
+
+    for (const file of files) {
+      const extension = file.name.includes(".")
+        ? "." + file.name.split(".").pop()
+        : "";
+
+      const fileName = `${crypto.randomUUID()}${extension}`;
+
+      const blob = await put(
+        `uploads/${fileName}`,
+        file,
+        {
+          access: "public",
+        }
+      );
+
+      urls.push(blob.url);
+    }
+
+    return NextResponse.json({
+      urls,
+    });
+  } catch (err) {
+    console.error("Upload error:", err);
+
+    return NextResponse.json(
+      {
+        error: "Failed to upload files",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
 }
